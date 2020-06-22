c7efabcb77b2d849aca3bbafb111a315597365eb
joker7t <batoan131196@gmail.com>
2020-06-22 23:43:08 +0700

    login and jwt for login

--- contact-keeper-server/routes/auth.js
+++ contact-keeper-server/routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const User = require('../models/User');
 
 // @route   GET api/auth
 // @desc    get logged in user
@@ -11,8 +16,43 @@ router.get('/', (req, res) => {
 // @route   POST api/auth
 // @desc    auth user and get token
 // @access  PRIVATE
-router.post('/', (req, res) => {
-    res.send('logged in user');
+router.post('/', [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            res.status(400).json({ msg: 'Invalid Credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            res.status(400).json({ msg: 'Invalid Credentials' });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (error, token) => {
+            if (error) throw error;
+            res.json({ token });
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error');
+    }
+
 });
 
 module.exports = router;
