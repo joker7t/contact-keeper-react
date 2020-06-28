0df8aec4352c8f34ea96b342d09e687e616bb66a
joker7t <batoan131196@gmail.com>
2020-06-28 23:00:54 +0700

    structure items in header

--- contact-keeper-client/src/components/layouts/Header.js
+++ contact-keeper-client/src/components/layouts/Header.js
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = ({ title, icon }) => {
+
+    const onLogout = () => {
+        console.log('logout');
+
+    }
+
     return (
         <Navbar expand="lg" bg='primary' variant='dark' >
             <Navbar.Brand as={Link} to="/">
@@ -12,11 +18,18 @@ const Header = ({ title, icon }) => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/about">About</Nav.Link>
-                </Nav>
-
+                {true ?
+                    <Nav className="ml-auto">
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link onClick={onLogout}>Logout</Nav.Link>
+                    </Nav>
+                }
             </Navbar.Collapse>
         </Navbar>
     );
